9e701c53c0dcbb72867d42a932a03908f1877a57
ricgt <147123245+ricgt@users.noreply.github.com>
2023-10-06 04:56:35 +0100

    Update vast8.sh

--- vast8.sh
+++ vast8.sh
@@ -45,7 +45,7 @@ sleep 5
 echo "---------------------------"
 
 # Update the configuration file
-sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0xca5F023af4F822353A563Ae6a3591bA2024495E8/g' config.conf
+sed -i 's/account = 0x24691e54afafe2416a8252097c9ca67557271475/account = 0xb52FFB2fC5DA23E3F51Eb1E9DF1C76a314700aF1/g' config.conf
 sleep 5
 echo "---------------------------"
 
